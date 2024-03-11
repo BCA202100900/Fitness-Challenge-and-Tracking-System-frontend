6dc098a0dd47f692d32f7071baa0ba0c4fe036c5
BCA202100900 <202100900@sigc.edu>
2024-03-11 04:04:45 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://cdn.outsideonline.com/wp-content/uploads/2023/10/fitness-tracker.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://fitness-challenge-and-tracking-system-backend.vercel.app"
